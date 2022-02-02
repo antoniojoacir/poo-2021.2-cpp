697bb3b02d10d8b2aceb6e5cd6b09e1970507dab
antoniojoacir <tungstenioiv@gmail.com>
2022-02-02 10:03:20 -0300

    correções 02/02

--- @013-cadastro/source.cpp
+++ @013-cadastro/source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <list>
+#include <map>
 
 using namespace std;
 
@@ -34,7 +35,7 @@ public:
         this->balance += value;
     }
 
-    bool transfer(shared_ptr<Account>& other, float value) {
+    bool transfer(Account * other, float value) {
         if(value < 0) {   
             cout <<"Error: Value cannot be negative\n";
             return false;
@@ -91,7 +92,7 @@ public:
 
     void mothyUpdate() override {
         // ? cada vez que essa função for chamada ela vai acrescentar 1% ao saldo
-        this->balance *= 0.01;
+        this->balance = this->balance + (this->balance * 0.01);
         cout << "Success: Operation performed successfully\n";
     }
 };
@@ -104,4 +105,123 @@ public:
     {
     }
     
+    friend ostream& operator<<(ostream&, const Client&);
+
+    list<shared_ptr<Account>> getAccounts() {
+        return this->accounts;
+    }
+
+    string getNameId() {
+        return this->nameId;
+    }
+
+    void addAccount(shared_ptr<Account> account) {
+        this->accounts.push_back(account);
+    }
 };
+ostream& operator<<(ostream& out, const Client& client) {
+    out << client.nameId << ": ";
+    for(auto account : client.accounts) {
+        out << *account;
+    }
+    return out;
+}
+
+class Bank {
+    map<int, shared_ptr<Account>> accounts; 
+    map<string, shared_ptr<Client>> clients;
+    int nextId { 0 };
+    Account* getAccount(int id) {
+        auto it = this->accounts.find(id);
+        if(it == this->accounts.end()) {
+            return nullptr;
+        }
+        return it->second.get();
+    }
+public:
+    Bank() {}
+
+    void addClient(string nameId) {
+        auto it = this->clients.find(nameId);
+        if(it != this->clients.end()) {
+            it->second->addAccount(make_shared<Cheking>(this->nextId++, nameId));
+            it->second->addAccount(make_shared<Saving>(this->nextId++, nameId));
+            auto accounts = it->second->getAccounts();
+            for(auto account : accounts) {
+                this->accounts.insert(pair<int, shared_ptr<Account>>(account->getId(), account));
+            }
+            return;
+        } else {
+            auto client = make_shared<Client>(nameId);
+            client->addAccount(make_shared<Cheking>(this->nextId++, nameId));
+            client->addAccount(make_shared<Saving>(this->nextId++, nameId));
+            this->clients.insert(make_pair(nameId, client));
+            auto accounts = client->getAccounts();
+            for(auto account : accounts) {
+                this->accounts.insert(pair<int, shared_ptr<Account>>(account->getId(), account));
+            }
+        }
+    }
+
+    bool deposit(int id, float value) {
+        auto account = this->getAccount(id);
+        if(account == nullptr) {
+            cout << "Error: Account not found\n";
+            return false;
+        }
+        account->deposit(value);
+        return true;
+    }
+
+    bool transfer(int id, int id2, float value) {
+        auto youSent = this->getAccount(id);
+        auto hasReceived = this->getAccount(id2);
+        if(youSent == nullptr || hasReceived == nullptr) {
+            cout << "Error: Account not found\n";
+            return false;
+        }
+        return youSent->transfer(hasReceived, value);
+    }
+
+    bool whitdraw(int id, float value) {
+        auto account = this->getAccount(id);
+        if(account == nullptr) {
+            cout << "Error: Account not found\n";
+            return false;
+        }
+        return account->withdraw(value);
+    }
+    
+    void mothyUpdate() {
+        for(auto& account : this->accounts) {
+            account.second->mothyUpdate();
+        }
+    }
+
+    friend ostream& operator<<(ostream&, const Bank&);
+};
+ostream& operator<<(ostream& out, const Bank& bank) {
+    out << "Clients: " << endl;
+    for(auto client : bank.clients) {
+        out << client.first << endl;
+    }
+    out << "Accounts: " << endl;
+    for(auto account : bank.accounts) {
+        out << *account.second;
+    }
+    return out;
+}
+
+
+int main() {
+    Bank bank;
+    bank.addClient("Joao");
+    bank.addClient("Maria");
+    bank.addClient("Joao");
+
+    bank.deposit(1, 100);
+    bank.transfer(1, 2, 50);
+    bank.whitdraw(2, 25);
+    bank.mothyUpdate();
+    cout << bank << endl;
+}
